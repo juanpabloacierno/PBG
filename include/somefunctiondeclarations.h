2580b5d40e0a737e1a6ba53cb5e78d6f3b422885
Juan Pablo Acierno <juanpabloacierno@gmail.com>
2013-12-04 20:17:42 -0400

    Create somefunctiondeclarations.h

---
+++ include/somefunctiondeclarations.h
@@ -0,0 +1,13 @@
+int parametersin();
+void endtoenddistance();
+int GetTargetBB(char *);
+int CheckMyRecord(char *, int *, int *, double *, double *, double *, char *, int *, char *);
+void coordforfitofends(int, int);
+int superimpose(long);
+double rmsdcalculation(int, int);
+int move (int);
+void coordforglobalfit(int, int);
+int WriteFitLibrary(char *, int, int);
+void C1C2andC8C9dis(int, int);
+int readlibraryfromfile();
+
