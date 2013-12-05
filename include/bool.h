68bfcc8f1b7e9fa800f371187f1e01c5ebd15aa8
Juan Pablo Acierno <juanpabloacierno@gmail.com>
2013-12-04 20:21:59 -0400

    Create bool.h

---
+++ include/bool.h
@@ -0,0 +1,35 @@
+/*
+************************************************************************
+*
+*   bool.h - definition of BOOL type, TRUE and FALSE
+*
+*   Copyright (c) 1994
+*
+*   ETH Zuerich
+*   Institut fuer Molekularbiologie und Biophysik
+*   ETH-Hoenggerberg
+*   CH-8093 Zuerich
+*
+*   SPECTROSPIN AG
+*   Industriestr. 26
+*   CH-8117 Faellanden
+*
+*   All Rights Reserved
+*
+*   Date of last modification : 94/06/02
+*   Pathname of SCCS file     : /sgiext/molmol/tools/include/SCCS/s.bool.h
+*   SCCS identification       : 1.1
+*
+************************************************************************
+*/
+
+#ifndef BOOL_DEFINED
+#define BOOL_DEFINED
+typedef int BOOL;
+#endif
+#ifndef FALSE
+#define FALSE 0
+#endif
+#ifndef TRUE
+#define TRUE 1
+#endif
